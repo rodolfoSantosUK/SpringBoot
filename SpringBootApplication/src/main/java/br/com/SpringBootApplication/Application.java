460c92684fb99af0b210c33459e724a20e0d84cd
Rodolfo <santoseuro2018@gmail.com>
2019-12-12 17:20:22 -0300

    initial

---
+++ SpringBootApplication/src/main/java/br/com/SpringBootApplication/Application.java
@@ -0,0 +1,20 @@
+package br.com.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+	@RequestMapping("/")
+	public String initialRest() {
+		return "Just testing";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
