931fc8899e33b466fd2454790a1cf1a0a4d1c5d8
rodolfoSantosUK <santoseuro2018@gmail.com>
2019-12-14 18:49:43 -0300

    Criando os métodos referentes aos métodos get, post, put e delete.

--- SpringBootApplication/src/main/java/br/com/controller/UserController.java
+++ SpringBootApplication/src/main/java/br/com/controller/UserController.java
@@ -1,10 +1,29 @@
 package br.com.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("users")
 public class UserController {
 
+    @GetMapping
+    public String getUser() {
+        return "get user was called";
+    }
 
+    @PostMapping
+    public String createUser() {
+        return "update user was called";
+    }
+
+    @PostMapping
+    public String updateUser() {
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "update user was called";
+    }
 
 }
